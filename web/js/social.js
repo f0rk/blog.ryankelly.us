842db04376e1adbd8b3c48a5fc6c054fe3a63080
Ryan Kelly <ryan@ryankelly.us>
2016-08-17 21:43:57 -0400

    more social media tweaks

---
+++ web/js/social.js
@@ -0,0 +1,43 @@
+/* Copyright 2016, Ryan Kelly. All Rights Reserved. */
+
+function twitter(elem) {
+
+    var getTweetText = function(name) {
+        var tags = document.getElementsByName(name);
+        if (tags.length > 0) {
+            return tags[0].getAttribute("content");
+        }
+
+        return null;
+    };
+
+    var tweetText = getTweetText("twitter:tweet");
+    if (tweetText === null) {
+        tweetText = getTweetText("twitter:title");
+    }
+
+    var base = "https://twitter.com/intent/tweet?";
+
+    var params = [];
+
+    params.push("original_referrer=" + encodeURIComponent(window.location.href));
+    params.push("ref_src=" + encodeURIComponent("twsrc^tfw"));
+    params.push("tw_p=" + encodeURIComponent("tweetbutton"));
+    params.push("url=" + encodeURIComponent(window.location.href));
+    params.push("text=" + encodeURIComponent(tweetText));
+
+    var url = base + params.join("&");
+
+    elem.setAttribute("href", url);
+}
+
+function socialize() {
+
+    var twitterButton = document.getElementById("twitter-button");
+    if (twitterButton !== null) {
+        twitter(twitterButton);
+    }
+
+}
+
+socialize();
