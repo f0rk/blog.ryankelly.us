dc146c55013869f6afa6e34140884875fbc69da9
Ryan Kelly <ryan@ryankelly.us>
2016-08-17 21:57:44 -0400

    more control over social link

--- web/js/social.js
+++ web/js/social.js
@@ -16,6 +16,20 @@ function twitter(elem) {
         tweetText = getTweetText("twitter:title");
     }
 
+    var getUrl = function() {
+        var tags = document.getElementsByName("twitter:url");
+        if (tags.length > 0) {
+            return tags[0].getAttribute("content");
+        }
+
+        tags = document.querySelectorAll('[property="og:url"]');
+        if (tags.length > 0) {
+            return tags[0].getAttribute("content");
+        }
+
+        return window.location.href;
+    };
+
     var base = "https://twitter.com/intent/tweet?";
 
     var params = [];
@@ -23,7 +37,7 @@ function twitter(elem) {
     params.push("original_referrer=" + encodeURIComponent(window.location.href));
     params.push("ref_src=" + encodeURIComponent("twsrc^tfw"));
     params.push("tw_p=" + encodeURIComponent("tweetbutton"));
-    params.push("url=" + encodeURIComponent(window.location.href));
+    params.push("url=" + encodeURIComponent(getUrl()));
     params.push("text=" + encodeURIComponent(tweetText));
 
     var url = base + params.join("&");
